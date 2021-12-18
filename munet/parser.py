72f9228c0d037bb64540c25827e6d719084d70bb
Christian Hopps <chopps@labn.net>
2021-12-18 09:01:28 -0500

    add config validation option

--- munet/parser.py
+++ munet/parser.py
@@ -176,6 +176,39 @@ ff02::2\tip6-allrouters
                 hf.write(f"{e[1]}\t{e[0]}\n")
 
 
+def validate_config(config, logger, args):
+    import jsonschema  # pylint: disable=C0415
+
+    from jsonschema.exceptions import ValidationError  # pylint: disable=C0415
+
+    if not config:
+        config = get_config(basename="munet")
+        del config["config_pathname"]
+
+    old = os.getcwd()
+    if args:
+        os.chdir(args.rundir)
+
+    try:
+        search = [old]
+        with importlib.resources.path("munet", "munet-schema.yaml") as datapath:
+            search.append(str(datapath.parent))
+
+        schema = get_config(basename="munet-schema", search=search)
+        jsonschema.validate(instance=config, schema=schema)
+        logger.info("Validated")
+        return True
+    except FileNotFoundError as error:
+        logger.info("No schema found: %s", error)
+        return False
+    except ValidationError as error:
+        logger.info("Validation failed: %s", error)
+        return False
+    finally:
+        if args:
+            os.chdir(old)
+
+
 def load_kinds(args):
     # Change CWD to the rundir prior to parsing config
     old = os.getcwd()
