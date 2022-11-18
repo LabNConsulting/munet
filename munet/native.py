5e676197e9af34c3d705248d42a7fbb6cfc3abfe
Christian Hopps <chopps@labn.net>
2022-11-18 03:16:08 +0000

    code: use sudo -u and port config
    add sudo

--- munet/native.py
+++ munet/native.py
@@ -2252,6 +2252,7 @@ class Munet(BaseMunet):
         elif config and config.get("server"):
             cls = SSHRemote
             kwargs["server"] = config["server"]
+            kwargs["port"] = int(config.get("port", 22))
         else:
             cls = L3Node
         return super().add_host(name, cls=cls, unet=self, config=config, **kwargs)
