e68709611e2b1ddaf4d4b3ce8a58298c60fdda53
Christian Hopps <chopps@labn.net>
2022-11-19 07:07:39 +0000

    tests: add sshremote node to spawn test

--- tests/control/test_spawn.py
+++ tests/control/test_spawn.py
@@ -29,20 +29,44 @@ import pytest
 # All tests are coroutines
 pytestmark = pytest.mark.asyncio
 
+# All tests are coroutines
+pytestmark = pytest.mark.asyncio
+if "GITHUB_ACTION" in os.environ:
+    pytestmark = pytest.mark.parametrize(
+        "unet_share", ["munet-ci"], indirect=["unet_share"]
+    )
+
 
 async def _test_repl(unet, hostname, cmd, use_pty, will_echo=False):
     host = unet.hosts[hostname]
     time.sleep(1)
-    repl = await host.console(
-        cmd, user="root", use_pty=use_pty, will_echo=will_echo, trace=True
-    )
+    if hasattr(host, "console"):
+        repl = await host.console(
+            cmd, user="root", use_pty=use_pty, will_echo=will_echo, trace=True
+        )
+    else:
+
+        lfname = os.path.join(unet.rundir, hostname + "-shell_spawn-log.txt")
+        logfile = open(lfname, "a+", encoding="utf-8")
+        logfile.write("-- start logging for: '{}' --\n".format(cmd))
+
+        prompt = r"(^|\r?\n)[^#\$]*[#\$] "
+        repl = await host.shell_spawn(
+            cmd,
+            prompt,
+            use_pty=use_pty,
+            will_echo=will_echo,
+            logfile=logfile,
+            trace=True,
+        )
     return repl
 
 
-@pytest.mark.parametrize("host", ["host1", "container1"])
+@pytest.mark.parametrize("host", ["host1", "container1", "remote1"])
 @pytest.mark.parametrize("mode", ["pty", "piped"])
 @pytest.mark.parametrize("shellcmd", ["/bin/bash", "/bin/dash", "/usr/bin/ksh"])
-async def test_spawn(unet, host, mode, shellcmd):
+async def test_spawn(unet_share, host, mode, shellcmd):
+    unet = unet_share
     if not os.path.exists(shellcmd):
         pytest.skip(f"{shellcmd} not installed skipping")
 
@@ -60,9 +84,10 @@ async def test_spawn(unet, host, mode, shellcmd):
         output = repl.cmd_raises("unset HISTFILE LSCOLORS")
         assert not output.strip()
 
-        output = repl.cmd_raises("env | grep TEST_SHELL")
-        logging.debug("'env | grep TEST_SHELL' output: %s", output)
-        assert output == f"TEST_SHELL={shellcmd}"
+        if host != "remote1":
+            output = repl.cmd_raises("env | grep TEST_SHELL")
+            logging.debug("'env | grep TEST_SHELL' output: %s", output)
+            assert output == f"TEST_SHELL={shellcmd}"
 
         expected = (
             "block\nbus\nclass\ndev\ndevices\nfirmware\nfs\nkernel\nmodule\npower"
