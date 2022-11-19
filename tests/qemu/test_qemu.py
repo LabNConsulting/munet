4d56435d5281c32d66af205935d0c84b5087444c
Christian Hopps <chopps@labn.net>
2022-11-19 23:02:58 +0000

    test: improve qemu test to actually test things

--- tests/qemu/test_qemu.py
+++ tests/qemu/test_qemu.py
@@ -39,4 +39,23 @@ async def fetch_images():
 async def test_qemu_up(unet):
     r1 = unet.hosts["r1"]
     output = r1.monrepl.cmd_nostatus("info status")
-    logging.debug("r1: kvm status: %s", output)
+    assert output == "VM status: running"
+
+
+async def test_net_up(unet):
+    h1 = unet.hosts["h1"]
+    r1 = unet.hosts["r1"]
+
+    h1mgmt0ip = h1.intf_addrs["eth0"].ip
+    r1mgmt0ip = r1.intf_addrs["eth0"].ip
+
+    h1r1ip = h1.intf_addrs["eth1"].ip
+    r1h1ip = r1.intf_addrs["eth1"].ip
+
+    logging.debug(h1.cmd_raises("ping -w1 -i.2 -c1 192.168.0.254"))
+    logging.debug(h1.cmd_raises(f"ping -w1 -i.2 -c1 {r1mgmt0ip}"))
+    logging.debug(h1.cmd_raises(f"ping -w1 -i.2 -c1 {r1h1ip}"))
+
+    logging.debug(r1.conrepl.cmd_raises("ping -w1 -i.2 -c1 192.168.0.254"))
+    logging.debug(r1.conrepl.cmd_raises(f"ping -w1 -i.2 -c1 {h1mgmt0ip}"))
+    logging.debug(r1.conrepl.cmd_raises(f"ping -w1 -i.2 -c1 {h1r1ip}"))
