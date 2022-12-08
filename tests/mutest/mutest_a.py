c26cb3aa7a1735516b332a5581a7448940b2a185
Christian Hopps <chopps@labn.net>
2022-12-07 21:51:50 -0500

    tests: add some actual waiting in the wait step test

--- tests/mutest/mutest_a.py
+++ tests/mutest/mutest_a.py
@@ -1,28 +1,30 @@
 """A file for testing send/expect steps"""
 # pylint: disable=line-too-long
-from munet.mutest.userapi import log, step, match_step, wait_step
-
-output = step("r1", "ls -l /")
-log("SIMPLE OUTPUT: %s", output)
+from munet.mutest.userapi import step, match_step, wait_step, TestCase
 
 # expect passing tests
-match_step("r1","ls -l /dev/tty", "crw.* /dev/tty", "Look for /dev/tty")
+match_step("r1", "ls -l /dev/tty", "crw.* /dev/tty", "Look for /dev/tty")
 match_step(target="r1",
            cmd="ls -l /dev/tty",
            match="crw.* /dev/tty",
            desc="Look for /dev/tty")
-wait_step("r1", "ls -l /dev/tty", "crw.* /dev/tty", "Look for /dev/tty", 1)
-wait_step("r1", "ls -l /dev/tty", "crw.* /dev/tty", "Look for /dev/tty", 1, False)
-wait_step("r1", "ls -l /dev/tty", "crw.* /dev/tty", "Look for /dev/tty", 1, .25)
-wait_step("r1", "ls -l /dev/tty", "crw.* /dev/tty", "Look for /dev/tty", 1, expect_fail=False)
-wait_step("r1", "ls -l /dev/tty", "crw.* /dev/tty", "Look for /dev/tty", 1, .25, False)
-wait_step("r1", "ls -l /dev/tty", "crw.* /dev/tty", "Look for /dev/tty", 1, .25, expect_fail=False)
+wait_step("r1", "sleep .2 && ls -l /dev/tty", "crw.* /dev/tty", "Look for /dev/tty", 1)
+wait_step("r1", "sleep .2 && ls -l /dev/tty", "crw.* /dev/tty", "Look for /dev/tty", 1, False)
+wait_step("r1", "sleep .5 && ls -l /dev/tty", "crw.* /dev/tty", "Look for /dev/tty", 1, .25)
+wait_step("r1", "sleep .2 && ls -l /dev/tty", "crw.* /dev/tty", "Look for /dev/tty", 1,
+          expect_fail=False)
+wait_step("r1", "sleep .5 && ls -l /dev/tty", "crw.* /dev/tty", "Look for /dev/tty", 1, .25, False)
+wait_step("r1", "sleep .5 && ls -l /dev/tty", "crw.* /dev/tty", "Look for /dev/tty", 1, .25,
+          expect_fail=False)
 
 # expect failing tests
-match_step("host1","ls -l /", " nodir", "Look for no /nodir", True)
-match_step("host1","ls -l /", " nodir", "Look for no /nodir", expect_fail=True)
+match_step("host1", "ls -l /", " nodir", "Look for no /nodir", True)
+match_step("host1", "ls -l /", " nodir", "Look for no /nodir", expect_fail=True)
 wait_step("host1", "ls -l /", " nodir", "Look for no /nodir", 1, expect_fail=True)
 wait_step("host1", "ls -l /", " nodir", "Look for no /nodir", 1, .25, True)
-wait_step("host1", "ls -l /", " nodir", "Look for no /nodir", 1, interval=.25, expect_fail=True)
-wait_step("host1", "ls -l /", " nodir", "Look for no /nodir", timeout=1, expect_fail=True)
-wait_step("host1", "ls -l /", " nodir", "Look for no /nodir", timeout=1, interval=.25, expect_fail=True)
+wait_step("host1", "ls -l /", " nodir", "Look for no /nodir", 1, interval=.25,
+          expect_fail=True)
+wait_step("host1", "ls -l /", " nodir", "Look for no /nodir", timeout=1,
+          expect_fail=True)
+wait_step("host1", "ls -l /", " nodir", "Look for no /nodir", timeout=1, interval=.25,
+          expect_fail=True)
