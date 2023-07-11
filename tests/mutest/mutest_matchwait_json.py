75ac8142da1cf58a6582438f7b0fcd60579c3195
lbrady <lbrady@labn.net>
2023-07-11 14:28:35 -0400

    tests: added new steps to test some exact_match use cases
    Signed-off-by: lbrady <lbrady@labn.net>

--- tests/mutest/mutest_matchwait_json.py
+++ tests/mutest/mutest_matchwait_json.py
@@ -130,3 +130,90 @@ wait_step_json(
 wait_step_json(
     "r1", """echo 'not json'""", '{ "name": "other" }', "Look for chopps", 1, 0.25, True
 )
+
+section("Test positive inexact match_step_json calls")
+
+match_step_json(
+    "r1",
+    """echo '{ "name": "chopps"}'""",
+    '{ "name": "chopps"}',
+    "Look for chopps: one field",
+    exact_match=False,
+)
+match_step_json(
+    "r1",
+    """echo '{ "year": "2023", "name": "chopps", "month": "july"}'""",
+    '{ "name": "chopps"}',
+    "Look for chopps: multiple fields",
+    exact_match=False,
+)
+match_step_json(
+    "r1",
+    """echo '{ "year": "2023", "entry": {"month": "july", "name": "chopps"}}'""",
+    '{ "entry": { "name": "chopps"}}',
+    "Look for chopps: nested fields",
+    exact_match=False,
+)
+wait_step_json(
+    "r1",
+    """echo '{ "year": "2023", "entry": {"month": "july", "name": "chopps"}}'""",
+    '{ "entry": { "name": "chopps"}}',
+    "Look for chopps: wait_step variant",
+    timeout=1,
+    interval=0.25,
+    exact_match=False,
+)
+
+
+section("Test negative (expect_fail) inexact match_step_json calls")
+
+match_step_json(
+    "r1",
+    """echo '{}'""",
+    '{ "name": "chopps"}',
+    "Look for no chopps: empty json",
+    expect_fail=True,
+    exact_match=False,
+)
+match_step_json(
+    "r1",
+    """echo '{ "year": "2023", "name": "not chopps", "month": "july"}'""",
+    '{ "name": "chopps"}',
+    "Look for no chopps: name changed",
+    expect_fail=True,
+    exact_match=False,
+)
+match_step_json(
+    "r1",
+    """echo '{ "year": "2023", "user": {"month": "july", "name": "chopps"}}'""",
+    '{ "entry": { "name": "chopps"}}',
+    "Look for no chopps: entry missing",
+    expect_fail=True,
+    exact_match=False,
+)
+match_step_json(
+    "r1",
+    """echo '{ "year": "2023", "entry": "none"}'""",
+    '{ "entry": { "name": "chopps"}}',
+    "Look for no chopps: entry type changed",
+    expect_fail=True,
+    exact_match=False,
+)
+match_step_json(
+    "r1",
+    """echo '{ "year": "2023", "entry": {"month": "july", "name": "not chopps"}}'""",
+    '{ "entry": { "name": "chopps"}}',
+    "Look for no chopps: nested name changed",
+    expect_fail=True,
+    exact_match=False,
+)
+wait_step_json(
+    "r1",
+    """echo '{ "year": "2023", "entry": {"month": "july", "name": "not chopps"}}'""",
+    '{ "entry": { "name": "chopps"}}',
+    "Look for no chopps: wait_step variant",
+    timeout=1,
+    interval=0.25,
+    expect_fail=True,
+    exact_match=False,
+)
