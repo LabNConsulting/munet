4c6cf07e49bb5e70915ba4b21511bca34386d1b2
Christian Hopps <chopps@labn.net>
2024-04-14 02:31:19 +0000

    examples: add generic example, move ospf example under frr/ospf

---
+++ examples/mutest/mutest_shell.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 eval: (blacken-mode 1) -*-
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# April 12 2024, Christian Hopps <chopps@labn.net>
+#
+# Copyright (c) 2024, LabN Consulting, L.L.C.
+"""Example mutest."""
+from munet.mutest.userapi import match_step
+from munet.mutest.userapi import section
+from munet.mutest.userapi import step
+from munet.mutest.userapi import wait_step
+
+
+section("Tests to show use of steps.")
+
+match_step(
+    "h1",
+    "ping -c1 10.0.1.2",
+    match="0% packet loss",
+    desc="h1 pings h2",
+)
+
+step("h1", "(sleep 2 && touch foo) &")
+
+wait_step(
+    "h1",
+    "ls",
+    "foo",
+    "found file foo",
+)
