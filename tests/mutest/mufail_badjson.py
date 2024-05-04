803b661775ffe3bc22972040731d911ef2fcb7f3
Christian Hopps <chopps@labn.net>
2024-05-04 03:56:47 +0000

    mutest: bad json always fails the test
    Even if the user expects failure, bad json input will fail the test.
    
    This was requested from users as the more useful functionality. Really,
    the only useful reason to support expect_fail on bad json is for testing
    the framework. Most users with expect_fail=True are looking at comparing
    some valid json and having the comparison fail, and they want bad input
    to still be flagged as a failure. If they want to test for bad json
    they can use a normal `match_step()` test variant and match against the
    expected "bad" json output.

---
+++ tests/mutest/mufail_badjson.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 eval: (blacken-mode 1) -*-
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# May 4 2024, Christian Hopps <chopps@labn.net>
+#
+# Copyright (c) 2024, LabN Consulting, L.L.C.
+#
+"""Test that bad json always fails.
+
+This test should always fail so in a automation setup one must
+run mutest nested in a mutest test and verify it fails.
+"""
+from munet.mutest.userapi import match_step_json
+
+
+jsonblank = "{}"
+jsonbad = '{"bad":"trailing-comma",}'
+
+_, ret = match_step_json(
+    "r1",
+    f"echo '{jsonblank}'",
+    jsonbad,
+    "Output json is blank, match json is bad, always fail (even with expect fail)",
+    expect_fail=True,
+)
+
+_, ret = match_step_json(
+    "r1",
+    f"echo '{jsonblank}'",
+    jsonbad,
+    "Output json is blank, match json is bad, always fail",
+    expect_fail=False,
+)
+
+_, ret = match_step_json(
+    "r1",
+    f"echo '{jsonbad}'",
+    jsonblank,
+    "Output json is bad, match json is blank, always fail (even with expect fail)",
+    expect_fail=True,
+)
+
+_, ret = match_step_json(
+    "r1",
+    f"echo '{jsonbad}'",
+    jsonblank,
+    "Output json is bad, match json is blank, always fail",
+    expect_fail=False,
+)
