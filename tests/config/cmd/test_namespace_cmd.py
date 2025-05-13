b310137be52196102ef7fc0448d8273fa1f49dcd
Liam Brady <lbrady@labn.net>
2025-05-13 13:47:39 -0400

    munet: new `*-file` config
    Allows a user to configure `cmd` or `initial-cmd`
    by reading from a file instead of specifying the
    full command within the munet.yaml config. This
    breaks up the config and should make it easier
    to work with in the case of router VMs, etc.
    
    The yang schema is also updated appropriately.
    
    Signed-off-by: Liam Brady <lbrady@labn.net>

---
+++ tests/config/cmd/test_namespace_cmd.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 eval: (blacken-mode 1) -*-
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# May 20 2025, Liam Brady <lbrady@labn.net>
+#
+# Copyright 2021, LabN Consulting, L.L.C.
+#
+"Testing of cmd and cmd-file config for L3NamespaceNode"
+import logging
+
+import pytest
+
+
+# All tests are coroutines
+pytestmark = pytest.mark.asyncio
+
+
+@pytest.mark.parametrize("host", ["h-shcmd", "h-shcmd-file", "h-pycmd", "h-pycmd-file",
+                                  "h-shcmd-file-noshebang"])
+async def test_config_cmd(unet_share, host):
+    unet = unet_share
+    rn = unet.hosts[host]
+    output = rn.cmd_raises("cat cmd*.out")
+    logging.debug("expects to find 'foo' in cmd.out; Found: %s", output)
+    assert "foo" in output
